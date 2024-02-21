bc9968cc76c808d4b586c1785ba1bd26e6f9087d
Mariano Graziano <mariano.graziano@gmail.com>
2024-02-21 11:38:16 -0300

    Vars vs Let

--- 01-fundamentos/app.js
+++
@@ -1,9 +0,0 @@
-// console.log (console.log('Hola Mundo') );
-
-let a = 10
-    b = 20
-    c = 30
-    x = a + b; 
-
-console.log(a, b, c, x);   
-// const c = 10; 
---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,23 @@
+// console.log (console.log('Hola Mundo') );
+// alert('Hola desde app.js');
+let a = 10,
+    b = 20,
+    c = 'Hola',
+    d = 'Spiderman',
+    x = a + b; 
+
+const saludo = c+ ' ' + d;
+
+// console.log(saludo);
+// console.log('%c Hola Mundo', 'color: red');
+// console.log({a, b, c, x});   
+// console.log('a ', a);
+// console.warn('Esto es un warning');
+// console.error(x);
+
+
+// console.table({a, b, c, x});
+
+c = 'Hola de nuevo';
+
+var MiNombre = 'Fernando';
