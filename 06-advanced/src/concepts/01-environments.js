1ffbb350725cbfa1fdd15ff730e2917336019c3d
Mariano Graziano <mariano.graziano@gmail.com>
2024-02-25 14:36:33 -0300

    Advanced

---
+++ 06-advanced/src/concepts/01-environments.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const environmentsComponent = ( element ) => {
+
+    console.log( import.meta.env) 
+const html = `
+    Dev: ${ import.meta.env.DEV } <br/>
+    KEY: ${ import.meta.env.VITE_API_KEY } <br/>
+    URL: ${ import.meta.env.VITE_BASE_URL } <br/>
+`;
+element.innerHTML = html; 
+}
